967d53dfad8d3d29deb85157be7eaa0f4a806f0c
kask <katarz.szcepaniak@gmail.com>
2025-09-26 12:42:12 +0200

    modified:   01_tensor_operations.py

--- 01_tensor_operations.py
+++ 01_tensor_operations.py
@@ -15,4 +15,17 @@ tensor3 = tensor2.transpose(dim0=0, dim1=1)
 
 tensor2[0,0] = 0
 print(tensor3)
-#print(torch.matmul(tensor1, tensor2.transpose(dim0=0, dim1=1)))
+print(torch.matmul(tensor1, tensor2.transpose(dim0=0, dim1=1)))
+
+print(tensor1.min())
+print(tensor2.min())
+print(tensor1.max())
+print(tensor2.max())
+#print(tensor1.mean())
+#print(tensor2.mean())
+print(tensor1.float().mean())
+print(tensor2.float().mean())
+print(tensor1.median())
+print(tensor2.median())
+print(tensor1.sum())
+print(tensor2.sum())
